5f7012f6c716731470dfc19f59c6ada677343baa
hariv <vngpln@yahoo.co.in>
2019-03-30 23:22:15 -0700

    Added basic pipeline code

---
+++ src/Pipeline.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+class Pipeline extends Component {
+  render() {
+    return (
+      <div className="Pipeline">
+      <header className="Pipeline-header">
+      <h1 className="heading">Pipeline Visualizer</h1>
+      <p>
+      <h3 className="subHeading">Define Pipeline</h3>
+      </p>
+      </header>
+      </div>
+	    );
+  }
+}
+
+export default Pipeline;
